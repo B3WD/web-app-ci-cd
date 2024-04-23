7c730d4bbe732d5ce32d9b6ea134f58fbf15a92e
Tsvetomir Pavlov <tsvetomir.kpavlov@gmail.com>
2024-04-23 22:15:39 +0300

    Update tests

--- test_main.py
+++ test_main.py
@@ -1,10 +1,19 @@
 from fastapi.testclient import TestClient
 from main import app
+from bs4 import BeautifulSoup
 
 client = TestClient(app)
 
 
 def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Hello World"}
+  response = client.get("/")
+  assert response.status_code == 200
+
+  # Parse the HTML content with BeautifulSoup
+  soup = BeautifulSoup(response.content, 'html.parser')
+
+  # Find the button element
+  button = soup.find('button')
+
+  # Assert the button text
+  assert button.text == "Hello world!"
